09ab41cca5d677e7b2e329f5503dcaf59f83e516
thotska <thotska@gmail.com>
2025-09-23 20:20:33 -0500

    ValidationOption2

--- Tests/DropdownTest.spec.ts
+++ Tests/DropdownTest.spec.ts
@@ -11,3 +11,13 @@ test("Selection of option", async ({page}) => {
     await dropdownPage.selectDropdownField("Option 1")
 })
 
+
+test('Validation of option 2 selection', async ({page}) => {
+    let homepage = new Homepage(page)
+    let dropdownPage = new DropdownPage(page)
+
+    await page.goto('https://the-internet.herokuapp.com/')
+    await homepage.clickOnDropdownLink()
+    await dropdownPage.selectDropdownField("Option 2")
+    await expect(dropdownPage.dropDownFieldLocator).toHaveValue("2")
+})
--- pages/dropdownPage.ts
+++ pages/dropdownPage.ts
@@ -5,11 +5,9 @@ export class DropdownPage{
     dropDownFieldLocator: Locator
     constructor(page: Page){
         this.dropDownFieldLocator = page.locator('select[id="dropdown"]')
-
          }
 
     async selectDropdownField(option: string): Promise<void>{
         this.dropDownFieldLocator.selectOption(option)
-
     }
 }
