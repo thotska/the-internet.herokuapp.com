bde4454ef515bd8e30cabd141df36505c049a742
thotska <thotska@gmail.com>
2025-09-25 20:50:35 -0500

    Before Each Test updated

--- tests/negativeInvalidUsername&Password.spec.ts
+++ tests/negativeInvalidUsername&Password.spec.ts
@@ -2,26 +2,30 @@ import {test} from '@playwright/test'
 import { Homepage} from '../pages/Homepage'
 import { FormAuthenticationPage } from '../pages/formAuthentication'
 
-test('Negative test: Validation of invalid username message', async ({page}) => {
-    let homepage = new Homepage(page)
-    let formAuthenticationPage = new FormAuthenticationPage(page)
+let homepage: Homepage
+let formAuthenticationPage: FormAuthenticationPage
 
-   await  page.goto('https://the-internet.herokuapp.com/')
-   await  homepage.clickOnFormAuthenticationLink()
+
+test.beforeEach(async ({page}) => {
+     homepage = new Homepage(page)
+     formAuthenticationPage = new FormAuthenticationPage(page)
+    await page.goto('https://the-internet.herokuapp.com/')
+    await  homepage.clickOnFormAuthenticationLink()
+    await  formAuthenticationPage.clickLogInButton()
+}
+)  
+
+test('Negative test: Validation of invalid username message', async ({page}) => {
+   
    await  formAuthenticationPage.fillUsernameField('invalid_username')
    await  formAuthenticationPage.fillPasswordField('SuperSecretPassword!')
-   await  formAuthenticationPage.clickLogInButton()
    await  formAuthenticationPage.invalidUsernameMessageVerification()
 })
 
 test('Negative test: Validation of invalid password message', async ({page}) => {
     let homepage = new Homepage(page)
     let formAuthenticationPage = new FormAuthenticationPage(page)
-
-   await  page.goto('https://the-internet.herokuapp.com/')
-   await  homepage.clickOnFormAuthenticationLink()
    await  formAuthenticationPage.fillUsernameField('tomsmith')
    await  formAuthenticationPage.fillPasswordField('invalid_password')
-   await  formAuthenticationPage.clickLogInButton()
    await  formAuthenticationPage.invalidPasswordMessageVerification()
 })
